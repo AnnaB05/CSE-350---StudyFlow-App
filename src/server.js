550cbb2cb525e0bc312e665669001d3ad7de2b9f
Anna Baker <aebake05@louisville.edu>
2026-09-17 14:52:09 -0400

    bp code added to server.js. this will be the main entry exucution file that mounts the structural database mappings then listens for inbound connections

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import app from './app.js';
+import { initializeDatabaseSchema } from './config/database.js';
+
+const PORT = 5000;
+
+async function bootServerEngine() {
+  // Sync the data structures cleanly prior to loading client interfaces
+  await initializeDatabaseSchema();
+
+  app.listen(PORT, () => {
+    console.log(`🚀 StudyFlow Application Core online and listening on network port: ${PORT}`);
+  });
+}
+
+bootServerEngine();
